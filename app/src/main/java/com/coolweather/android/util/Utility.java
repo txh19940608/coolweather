9f24f27dd236ad1d1a11e9d786e14d0b4eb27710
Tan <txh19940608@163.com>
2017-04-07 20:05:03 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,6 +18,23 @@ import org.json.JSONObject;
  */
 
 public class Utility {
+
+
+    /**
+     * 将返回的json数据解析成Weather
+     */
+    public static Weather handleWeatherResponse(String  response){
+
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     /**
      * 解析和处理服务返回的省级数据
      */
